d2dbb3aea3d8bdddda7d265008412b58e38a6bb4
CHETAN S/IHQ/TECH/TS <124719@ecomexpress.in>
2025-07-03 16:34:09 +0530

    Project Added

--- mongo.py
+++ mongo.py
@@ -1,10 +1,10 @@
 from pymongo import MongoClient
 
 # Your connection string (make sure the password is correct)
-client = MongoClient("mongodb+srv://chetan:Limit%123456789@cluster1.ccggsge.mongodb.net/")
+client = MongoClient("mongodb://chetan:Limit%123456789@ac-Cluster1-shard-00-00.cluster0.mongodb.net:27017,ac-Cluster1-shard-00-01.cluster0.mongodb.net:27017,ac-Cluster1-shard-00-02.cluster0.mongodb.net:27017/?ssl=true&replicaSet=atlas-Cluster1-shard-0&authSource=admin&retryWrites=true&w=majority")
 
 # Access a database
-db = client["test"]  # replace 'test' with your database name
+db = client["students_data"]  # replace 'test' with your database name
 
 # Example operation
 print(db.list_collection_names())
